5ae6447c20ac562953ba7f531867c47737edab5c
Léo Genot <leogenot@gmail.com>
2022-11-27 12:25:34 +0100

    removed unnecessary

--- src/templates/components/Map/MapBase.vue
+++ src/templates/components/Map/MapBase.vue
@@ -1,50 +1,23 @@
 <template>
     <div class="c-map" ref="content">
-        <button-primary
-            class="c-project-modal-btn"
-            label="Map"
-            color="light"
-            @click="openModal"
-        />
         <div class="c-map__map" ref="mapContainer" />
     </div>
 </template>
 
 <script>
 import { defineComponent, computed, ref, onMounted } from "vue";
-import { useStore } from "vuex";
-import { MODAL_COMPONENTS } from "@/constants";
 
 import "leaflet/dist/leaflet.css";
 import "leaflet-geosearch/dist/geosearch.css";
-import {
-    GeoSearchControl,
-    OpenStreetMapProvider,
-    GeoApiFrProvider,
-} from "leaflet-geosearch";
+import { GeoSearchControl, GeoApiFrProvider } from "leaflet-geosearch";
 import L from "leaflet";
-
-import ButtonPrimary from "@/templates/components/_buttons/ButtonPrimary.vue";
 
 export default defineComponent({
     name: "MapBase",
-    components: {
-        ButtonPrimary,
-    },
+    components: {},
     setup() {
-        const store = useStore();
         const content = ref(null);
         const mapContainer = ref(null);
-
-        const center = computed(() => {
-            return [43.664924, 5.156901];
-        });
-        function openModal(data) {
-            store.dispatch("modal/open", {
-                component: MODAL_COMPONENTS.Project,
-                data,
-            });
-        }
 
         // ----- Mapbox -----
         //TODO: Move to store when request form input in frontend, send to firebase functions then get result
@@ -72,7 +45,6 @@ export default defineComponent({
         });
 
         return {
-            openModal,
             content,
             mapContainer,
         };
