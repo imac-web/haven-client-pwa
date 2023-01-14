4f1335514329a1b1c12f8c675842bf2ae70145c5
Léo Genot <45610291+leogenot@users.noreply.github.com>
2023-01-14 22:41:35 +0100

    quick refactor

---
+++ src/templates/components/molecules/DraggableList/DraggableList.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="m-draggable-list">
+    <Sortable
+      :list="elements"
+      item-key="id"
+      :options="options"
+      @change="logEvent"
+      @choose="logEvent"
+      @unchoose="logEvent"
+      @start="logEvent"
+      @end="logEvent"
+      @add="logEvent"
+      @update="logEvent"
+      @sort="logEvent"
+      @remove="logEvent"
+      @filter="logEvent"
+      @move="logEvent"
+      @clone="logEvent"
+    >
+      <template #item="{ element, index }">
+        <div class="draggable" :key="element.id">
+          {{ element.text }}
+          <Sortable
+            v-if="element.children"
+            :list="element.children"
+            :item-key="(item) => item.id"
+            :options="options"
+            @change="logEvent"
+            @choose="logEvent"
+            @unchoose="logEvent"
+            @start="logEvent"
+            @end="logEvent"
+            @add="logEvent"
+            @update="logEvent"
+            @sort="logEvent"
+            @remove="logEvent"
+            @filter="logEvent"
+            @move="logEvent"
+            @clone="logEvent"
+          >
+            <template #item="{ element, index }">
+              <div class="draggable" :key="element.id">
+                {{ element.text }}
+              </div>
+            </template>
+          </Sortable>
+        </div>
+      </template>
+    </Sortable>
+  </div>
+</template>
+
+<script>
+import { defineComponent, toRef, computed, onMounted, ref } from "vue";
+import { Sortable } from "sortablejs-vue3";
+
+export default defineComponent({
+  name: "DraggableList",
+  components: {
+    Sortable,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+      default: {},
+    },
+  },
+  setup(props) {
+    const data = toRef(props, "data");
+
+    const elements = computed(() => {
+      return [
+        {
+          id: "1",
+          text: "One",
+          children: [
+            {
+              id: "1-1",
+              text: "One-One",
+              children: [
+                {
+                  id: "1-1-1",
+                  text: "One-One-One",
+                },
+                {
+                  id: "1-1-2",
+                  text: "One-One-Two",
+                },
+              ],
+            },
+            {
+              id: "1-2",
+              text: "One-Two",
+            },
+          ],
+        },
+        {
+          id: "2",
+          text: "Two",
+        },
+        {
+          id: "3",
+          text: "Three",
+        },
+        {
+          id: "4",
+          text: "Four",
+        },
+      ];
+    });
+    const logEvent = (evt, evt2) => {
+      if (evt2) {
+        //console.log(evt, evt2);
+      } else {
+        //console.log(evt);
+      }
+    };
+    const animating = ref(true);
+    const scrollSensitivity = ref(50);
+    const scrollSpeed = ref(10);
+    const options = computed(() => {
+      return {
+        draggable: ".draggable",
+        animation: animating.value ? 150 : 0,
+        ghostClass: "ghost",
+        dragClass: "drag",
+        scroll: true,
+        forceFallback: true,
+        scrollSensitivity: scrollSensitivity.value,
+        scrollSpeed: scrollSpeed.value,
+        bubbleScroll: true,
+      };
+    });
+
+    return {
+      data,
+      elements,
+      logEvent,
+      options,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.m-draggable-list {
+}
+
+.draggable {
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid var(--color-white);
+  cursor: move;
+}
+.ghost {
+  opacity: 0.5;
+  background: var(--color-white);
+  border: 1px dashed var(--color-white);
+}
+.drag {
+  background: var(--color-white);
+  color: var(--color-dark);
+}
+</style>
--- src/templates/components/organisms/Panels/PanelComponent.vue
+++ src/templates/components/organisms/Panels/PanelComponent.vue
@@ -8,53 +8,7 @@
       <div class="o-nameof-panel__wrapper-number">90%</div>
       <hr />
       <div class="o-nameof-panel__wrapper-list">
-        <Sortable
-          :list="elements"
-          item-key="id"
-          :options="options"
-          @change="logEvent"
-          @choose="logEvent"
-          @unchoose="logEvent"
-          @start="logEvent"
-          @end="logEvent"
-          @add="logEvent"
-          @update="logEvent"
-          @sort="logEvent"
-          @remove="logEvent"
-          @filter="logEvent"
-          @move="logEvent"
-          @clone="logEvent"
-        >
-          <template #item="{ element, index }">
-            <div class="draggable" :key="element.id">
-              {{ element.text }}
-              <Sortable
-                v-if="element.children"
-                :list="element.children"
-                :item-key="(item) => item.id"
-                :options="options"
-                @change="logEvent"
-                @choose="logEvent"
-                @unchoose="logEvent"
-                @start="logEvent"
-                @end="logEvent"
-                @add="logEvent"
-                @update="logEvent"
-                @sort="logEvent"
-                @remove="logEvent"
-                @filter="logEvent"
-                @move="logEvent"
-                @clone="logEvent"
-              >
-                <template #item="{ element, index }">
-                  <div class="draggable" :key="element.id">
-                    {{ element.text }}
-                  </div>
-                </template>
-              </Sortable>
-            </div>
-          </template>
-        </Sortable>
+        <DraggableList />
       </div>
     </div>
   </div>
@@ -63,12 +17,12 @@
 <script>
 import { defineComponent, toRef, computed, onMounted, ref } from "vue";
 import { useStore } from "vuex";
-import { Sortable } from "sortablejs-vue3";
+import DraggableList from "@/templates/components/molecules/DraggableList/DraggableList.vue";
 
 export default defineComponent({
   name: "ProjectPanel",
   components: {
-    Sortable,
+    DraggableList,
   },
   props: {
     data: {
@@ -89,77 +43,10 @@ export default defineComponent({
 
     const data = toRef(props, "data");
 
-    const elements = computed(() => {
-      return [
-        {
-          id: "1",
-          text: "One",
-          children: [
-            {
-              id: "1-1",
-              text: "One-One",
-              children: [
-                {
-                  id: "1-1-1",
-                  text: "One-One-One",
-                },
-                {
-                  id: "1-1-2",
-                  text: "One-One-Two",
-                },
-              ],
-            },
-            {
-              id: "1-2",
-              text: "One-Two",
-            },
-          ],
-        },
-        {
-          id: "2",
-          text: "Two",
-        },
-        {
-          id: "3",
-          text: "Three",
-        },
-        {
-          id: "4",
-          text: "Four",
-        },
-      ];
-    });
-    const logEvent = (evt, evt2) => {
-      if (evt2) {
-        //console.log(evt, evt2);
-      } else {
-        //console.log(evt);
-      }
-    };
-    const animating = ref(true);
-    const scrollSensitivity = ref(50);
-    const scrollSpeed = ref(10);
-    const options = computed(() => {
-      return {
-        draggable: ".draggable",
-        animation: animating.value ? 150 : 0,
-        ghostClass: "ghost",
-        dragClass: "drag",
-        scroll: true,
-        forceFallback: true,
-        scrollSensitivity: scrollSensitivity.value,
-        scrollSpeed: scrollSpeed.value,
-        bubbleScroll: true,
-      };
-    });
-
     return {
       close,
       data,
       isOpen,
-      elements,
-      logEvent,
-      options,
     };
   },
 });
@@ -191,21 +78,5 @@ export default defineComponent({
       overflow-y: auto;
     }
   }
-}
-
-.draggable {
-  padding: 10px;
-  margin: 10px;
-  border: 1px solid var(--color-white);
-  cursor: move;
-}
-.ghost {
-  opacity: 0.5;
-  background: var(--color-white);
-  border: 1px dashed var(--color-white);
-}
-.drag {
-  background: var(--color-white);
-  color: var(--color-dark);
 }
 </style>
