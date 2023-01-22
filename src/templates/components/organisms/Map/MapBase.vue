8058d795da8cce235ce4d7f7fcdb5a346dd34884
Léo Genot <45610291+leogenot@users.noreply.github.com>
2023-01-22 11:02:43 +0100

    new try

--- src/templates/components/organisms/Map/MapBase.vue
+++ src/templates/components/organisms/Map/MapBase.vue
@@ -17,9 +17,9 @@ L.Icon.Default.imagePath = ".";
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "leaflet/dist/images/marker-icon-2x.png",
-  iconUrl: "leaflet/dist/images/marker-icon.png",
-  shadowUrl: "leaflet/dist/images/marker-shadow.png",
+  iconRetinaUrl: "node_modules/leaflet/dist/images/marker-icon-2x.png",
+  iconUrl: "node_modules/leaflet/dist/images/marker-icon.png",
+  shadowUrl: "node_modules/leaflet/dist/images/marker-shadow.png",
 });
 
 import emitter from "@/services/emitter";
