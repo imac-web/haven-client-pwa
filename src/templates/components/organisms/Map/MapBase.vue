a503ccff786937ecaf4ebbbd30e636a419b0f8d6
Léo Genot <45610291+leogenot@users.noreply.github.com>
2023-01-22 11:20:19 +0100

    added filled icon for map

--- src/templates/components/organisms/Map/MapBase.vue
+++ src/templates/components/organisms/Map/MapBase.vue
@@ -25,7 +25,7 @@ export default defineComponent({
     let map = null;
     let marker = null;
     const markerIcon = L.icon({
-      iconUrl: "static/images/pinpoint.svg",
+      iconUrl: "static/images/pinpoint-filled.svg",
       iconSize: [48, 48], // size of the icon
       iconAnchor: [15.5, 42], // point of the icon which will correspond to marker's location
       popupAnchor: [0, -45], // point from which the popup should open relative to the iconAnchor
