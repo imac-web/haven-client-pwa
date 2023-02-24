ba9e8c200ac688699b8344d243a679d651555cb1
Léo Genot <45610291+leogenot@users.noreply.github.com>
2023-02-24 13:15:39 +0100

    added layers switching for map

--- src/templates/components/organisms/Map/MapBase.vue
+++ src/templates/components/organisms/Map/MapBase.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="o-map" ref="content">
-        <div class="o-map__map" ref="mapContainer" />
-    </div>
+  <div class="o-map" ref="content">
+    <div class="o-map__map" ref="mapContainer" />
+  </div>
 </template>
 
 <script>
@@ -16,139 +16,154 @@ import L, { icon, map } from "leaflet";
 import emitter from "@/services/emitter";
 
 export default defineComponent({
-    name: "MapBase",
-    components: {},
-    emits: ["SlideDownPanel", "SelectedLocation"],
-    setup(props, { emit }) {
-        const content = ref(null);
-        const mapContainer = ref(null);
-        let map = null;
-        let marker = null;
-        const markerIcon = L.icon({
-            iconUrl:
-                "https://raw.githubusercontent.com/imac-web/haven-client-pwa/main/static/images/pinpoint-filled.svg",
-            iconSize: [48, 48], // size of the icon
-            iconAnchor: [15.5, 42], // point of the icon which will correspond to marker's location
-            popupAnchor: [0, -45], // point from which the popup should open relative to the iconAnchor
-        });
-        const SelectedResult = ref([]);
+  name: "MapBase",
+  components: {},
+  emits: ["SlideDownPanel", "SelectedLocation"],
+  setup(props, { emit }) {
+    const content = ref(null);
+    const mapContainer = ref(null);
+    let map = null;
+    let marker = null;
+    const markerIcon = L.icon({
+      iconUrl:
+        "https://raw.githubusercontent.com/imac-web/haven-client-pwa/main/static/images/pinpoint-filled.svg",
+      iconSize: [48, 48], // size of the icon
+      iconAnchor: [15.5, 42], // point of the icon which will correspond to marker's location
+      popupAnchor: [0, -45], // point from which the popup should open relative to the iconAnchor
+    });
+    const SelectedResult = ref([]);
 
-        emitter.on("selected-result", (data) => {
-            emitter.emit("slide-down-panel", true);
-            SelectedResult.value = data;
-            map.setView(
-                [
-                    SelectedResult.value.raw.geometry.coordinates[1],
-                    SelectedResult.value.raw.geometry.coordinates[0],
-                ],
-                13
-            );
-            marker = L.marker(
-                [
-                    SelectedResult.value.raw.geometry.coordinates[1],
-                    SelectedResult.value.raw.geometry.coordinates[0],
-                ],
-                {
-                    icon: markerIcon,
-                }
-            ).addTo(map);
-        });
-
-        // ----- Mapbox -----
-        function setupGeoSearch() {
-            map = L.map(mapContainer.value, { zoomControl: false }).setView(
-                [48.84277323737967, 2.587709798324433],
-                13
-            );
-
-            // L.tileLayer("//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-            L.tileLayer(
-                "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-                {
-                    maxZoom: 20,
-                    attribution:
-                        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-                }
-            ).addTo(map);
-            var popup = L.popup();
-
-            //need to fix bottom placement
-            //L.control.zoom({position: "bottomright",}).addTo(map);
-            L.control
-                .zoom({
-                    position: "topleft",
-                })
-                .addTo(map);
-
-            //open and close panel functions
-            const store = useStore();
-
-            function openPanel(data, index) {
-                store.dispatch("panel/open", {
-                    component: PANEL_COMPONENTS.Panel,
-                    data,
-                    index,
-                });
-            }
-            function openPanelMobile(data, index) {
-                store.dispatch("panelMobile/open", {
-                    component: PANEL_COMPONENTS.PanelMobile,
-                    data,
-                    index,
-                });
-            }
-
-            const close = () => {
-                store.dispatch("panel/close");
-            };
-
-            async function onMapClick(e) {
-                emitter.emit("selected-location", e.latlng);
-                popup
-                    .setLatLng(e.latlng)
-                    .setContent("You clicked the map at " + e.latlng.toString())
-                    .openOn(map);
-                let location = e.latlng;
-                let services = await getServices(
-                    location.lat,
-                    location.lng,
-                    1000
-                )
-                    .then((data) => {
-                        return data;
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-                openPanel(location, services);
-                openPanelMobile(location, services);
-            }
-
-            map.on("click", onMapClick);
+    emitter.on("selected-result", (data) => {
+      emitter.emit("slide-down-panel", true);
+      SelectedResult.value = data;
+      map.setView(
+        [
+          SelectedResult.value.raw.geometry.coordinates[1],
+          SelectedResult.value.raw.geometry.coordinates[0],
+        ],
+        13
+      );
+      marker = L.marker(
+        [
+          SelectedResult.value.raw.geometry.coordinates[1],
+          SelectedResult.value.raw.geometry.coordinates[0],
+        ],
+        {
+          icon: markerIcon,
         }
+      ).addTo(map);
+    });
 
-        onMounted(() => {
-            setupGeoSearch();
+    // ----- Mapbox -----
+    function setupGeoSearch() {
+      var dark = L.tileLayer(
+        "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+        {
+          maxZoom: 20,
+          attribution:
+            '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+        }
+      );
+
+      var osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "© OpenStreetMap",
+      });
+
+      var satelite = L.tileLayer(
+        "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+        {
+          attribution:
+            "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+        }
+      );
+
+      map = L.map(mapContainer.value, {
+        zoomControl: false,
+        layers: [dark, osm, satelite],
+      }).setView([48.84277323737967, 2.587709798324433], 13);
+
+      var baseMaps = {
+        OpenStreetMap: osm,
+        Satelite: satelite,
+        Dark: dark,
+      };
+
+      var layerControl = L.control.layers(baseMaps).addTo(map);
+
+      var popup = L.popup();
+
+      L.control
+        .zoom({
+          position: "topleft",
+        })
+        .addTo(map);
+
+      //open and close panel functions
+      const store = useStore();
+
+      function openPanel(data, index) {
+        store.dispatch("panel/open", {
+          component: PANEL_COMPONENTS.Panel,
+          data,
+          index,
         });
+      }
+      function openPanelMobile(data, index) {
+        store.dispatch("panelMobile/open", {
+          component: PANEL_COMPONENTS.PanelMobile,
+          data,
+          index,
+        });
+      }
 
-        return {
-            content,
-            mapContainer,
-            SelectedResult,
-        };
-    },
+      const close = () => {
+        store.dispatch("panel/close");
+      };
+
+      async function onMapClick(e) {
+        emitter.emit("selected-location", e.latlng);
+        popup
+          .setLatLng(e.latlng)
+          .setContent("You clicked the map at " + e.latlng.toString())
+          .openOn(map);
+        let location = e.latlng;
+        let services = await getServices(location.lat, location.lng, 1000)
+          .then((data) => {
+            return data;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        openPanel(location, services);
+        openPanelMobile(location, services);
+      }
+
+      map.on("click", onMapClick);
+    }
+
+    onMounted(() => {
+      setupGeoSearch();
+    });
+
+    return {
+      content,
+      mapContainer,
+      SelectedResult,
+    };
+  },
 });
 </script>
 
 <style lang="scss">
 .o-map {
-    z-index: 0;
-    &__map {
-        max-height: 100vh;
-        height: 100vh;
-        @include min(md) {
-            //height: 50vh;
-        }
+  z-index: 0;
+  &__map {
+    max-height: 100vh;
+    height: 100vh;
+    @include min(md) {
+      //height: 50vh;
     }
+  }
 }
 </style>
