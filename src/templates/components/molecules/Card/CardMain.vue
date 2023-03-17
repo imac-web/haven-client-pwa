ce1a196a738b1fc00a078dbc582a5d2f68bbaaf5
Léo Genot <45610291+leogenot@users.noreply.github.com>
2023-03-17 19:11:40 +0100

    loading state

--- src/templates/components/molecules/Card/CardMain.vue
+++ src/templates/components/molecules/Card/CardMain.vue
@@ -1,25 +1,26 @@
 <template>
-    <div class="m-card-main">
-        <div class="m-card-main__content">
-            <div class="m-card-main__content-top">
-                <h3>Vivability</h3>
-            </div>
-            <div class="m-card-main__content-bottom">
-                <ve-progress
-                    :progress="score * 10"
-                    :angle="90"
-                    :size="160"
-                    emptyColor="rgba(255, 255, 255, 0.1)"
-                    :thickness="15"
-                    :empty-thickness="15"
-                    :legend="score"
-                    :legend-formatter="myFormatter"
-                    legend-class="legend-custom-style"
-                    color="#25c685"
-                ></ve-progress>
-            </div>
-        </div>
+  <div class="m-card-main">
+    <div class="m-card-main__content">
+      <div class="m-card-main__content-top">
+        <h3>Vivability</h3>
+      </div>
+      <div class="m-card-main__content-bottom">
+        <ve-progress
+          :loading="loading"
+          :progress="score * 10"
+          :angle="90"
+          :size="160"
+          emptyColor="rgba(255, 255, 255, 0.1)"
+          :thickness="15"
+          :empty-thickness="15"
+          :legend="score"
+          :legend-formatter="myFormatter"
+          legend-class="legend-custom-style"
+          color="#25c685"
+        ></ve-progress>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -29,75 +30,81 @@ import { VeProgress } from "vue-ellipse-progress";
 import ButtonPrimary from "@/templates/components/atoms/_buttons/ButtonPrimary.vue";
 
 export default defineComponent({
-    name: "CardMain",
-    components: {
-        VeProgress,
-        ButtonPrimary,
+  name: "CardMain",
+  components: {
+    VeProgress,
+    ButtonPrimary,
+  },
+  props: {
+    score: {
+      type: Number,
+      required: false,
     },
-    props: {
-        score: {
-            type: Number,
-            required: true,
-        },
+    loading: {
+      type: Boolean,
+      required: true,
     },
-    setup(props) {
-        const score = toRef(props, "score");
+  },
+  setup(props) {
+    const score = toRef(props, "score");
+    const loading = toRef(props, "loading");
 
-        function setToFixed(v) {
-            return v % 1 ? v.toFixed(1) : v;
-        }
+    function setToFixed(v) {
+      return v % 1 ? v.toFixed(1) : v;
+    }
 
-        const myFormatter = ({ currentValue }) => {
-            return `
+    const myFormatter = ({ currentValue }) => {
+      return `
         <span style="font-weight: bold; font-size: 6rem;">${currentValue}</span>
         <span style="opacity:0.5; font-size: 2rem; text-transform:uppercase;"> / 10</span>
       `;
-        };
+    };
 
-        return {
-            score,
-            setToFixed,
-            myFormatter,
-        };
-    },
+    return {
+      score,
+      loading,
+      setToFixed,
+      myFormatter,
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 .m-card-main {
-    --bg-color: var(--color-haven_dark_grey);
-    background-color: var(--bg-color);
-    border-radius: (2rem);
+  --bg-color: var(--color-haven_dark_grey);
+  background-color: var(--bg-color);
+  border-radius: (2rem);
 
-    height: 30rem;
-    width: 100%;
-    padding: 1rem;
+  height: 30rem;
+  width: 100%;
+  padding: 1rem;
 
-    &__content {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        &-top {
-            text-align: center;
-        }
-        &-bottom {
-            text-align: center;
-        }
-        &-right {
-            position: absolute;
-            right: 0;
-            top: 0;
-            &-btn {
-                --color-white-alpha: rgba(255, 255, 255, 0.15);
-                --btn-txt-display: none;
-                --btn-bg-color: var(--color-white-alpha);
-                --btn-border-color: transparent;
-                --btn-padding-v: 0.5em;
-                --btn-padding-h: 0.5em;
-                --btn-svg-width: 0.7em;
-            }
-        }
+  &__content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    &-top {
+      text-align: center;
     }
+    &-bottom {
+      text-align: center;
+    }
+    &-right {
+      position: absolute;
+      right: 0;
+      top: 0;
+      &-btn {
+        --color-white-alpha: rgba(255, 255, 255, 0.15);
+        --btn-txt-display: none;
+        --btn-bg-color: var(--color-white-alpha);
+        --btn-border-color: transparent;
+        --btn-padding-v: 0.5em;
+        --btn-padding-h: 0.5em;
+        --btn-svg-width: 0.7em;
+      }
+    }
+  }
 }
 </style>
