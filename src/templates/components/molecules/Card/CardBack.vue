538be580507a2a8652a12425abdceee6026871fd
Léo Genot <45610291+leogenot@users.noreply.github.com>
2023-03-19 15:09:18 +0100

    added key

--- src/templates/components/molecules/Card/CardBack.vue
+++ src/templates/components/molecules/Card/CardBack.vue
@@ -3,7 +3,7 @@
     <div class="m-card-back__content">
       <div class="m-card-back__content-title">Détails de la note</div>
       <table class="m-card-back__content-table">
-        <tr v-for="(data, i) in data">
+        <tr v-for="(data, i) in data" :key="`detail-${i}`">
           <td v-if="data.label && data.score">{{ data.label }}</td>
 
           <td v-if="data.score">{{ data.score }}</td>
