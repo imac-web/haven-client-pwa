6792e9193e89abaf9ca0bbeee52bad5ac4bc3790
Léo Genot <45610291+leogenot@users.noreply.github.com>
2023-03-26 13:36:41 +0200

    added vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import { VitePWA } from 'vite-plugin-pwa'
 import path from "path"
 
 
@@ -13,6 +14,37 @@ export default defineConfig({
             symbolId: "icon-[dir]-[name]",
             customDomId: "spritesheet",
         }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            includeAssets: ['favicon.png'],
+            manifest: {
+                name: "Haven - Score d'habitabilité",
+                short_name: 'Haven',
+                description: "Haven - Score d'habitabilité",
+                theme_color: '#000',
+                icons: [
+                    {
+                        src: 'icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        })
     ],
     css: {
         preprocessorOptions: {
@@ -22,9 +54,6 @@ export default defineConfig({
         },
         devSourcemap: true,
     },
-    base: "/",
-    build: {
-        manifest: true,
-    },
+    base: "/"
 
 })
